54084e84cc60e610c2ac21786e65571e10962368
Foekens <128739@student.drenthecollege.nl>
2022-06-29 13:25:39 +0200

    opdracht 3 gemaakt

--- opdracht2.php
+++ opdracht2.php
@@ -1,18 +1,28 @@
-
 <?php
 $servername = "localhost";
-$databasename = "database2";
+$databasename = "db_level2_opdr1";
 $username = "user1";
 $password = "g8bal28";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $databasename);
+$query = 'SELECT * FROM `songs`';
+
 
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully";
+echo "Connected successfully" . '<br>';
 
+if ($result = $conn -> query($query)) {
+    while($row = $result->fetch_assoc()){
+        echo $row['id'] . ' ';
+        echo $row['artist'] .  ', ';
+        echo $row['title'] . '<br />';
+    }
+    
+    $result -> free_result();
+}
 
 ?>
